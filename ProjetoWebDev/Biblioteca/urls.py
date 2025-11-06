fac5dce56868bff643cd02606df326ec26c6bf8f
Fernando Ryuji Alves Ono <fryuji@rust.linux.ime.usp.br>
2025-11-06 18:32:54 -0300

    Teste da funcao criar_livro, a partir de um request do formulario e depois tentando substituir o conteudo de uma div com os objetos. Entretanto, neste momento toda a pagina html esta sendo substituida pela lista de objetos Livro.

--- ProjetoWebDev/Biblioteca/urls.py
+++ ProjetoWebDev/Biblioteca/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("",       views.index,       name="index"),
+    path("criar/", views.criar_livro, name="criar_livro")
 ]
+
--- ProjetoWebDev/Biblioteca/views.py
+++ ProjetoWebDev/Biblioteca/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Livro
 
 # Crie suas views aqui.
 
 def index(request):
-    return render(request, "Biblioteca/pesquisa.html")
+    livros = Livro.objects.all()
+    return render(request, "Biblioteca/gerenciamento.html", {"livros": livros})
 
+def criar_livro(request):    
+    if request.method == "POST":
+        titulo = request.POST.get("titulo")
+        autor = request.POST.get("autor")
+        genero = request.POST.get("genero")
+        sinopse = request.POST.get("sinopse")
+        ano = request.POST.get("ano")
 
+        livro = Livro(titulo=titulo, genero=genero, sinopse=sinopse, ano_de_publicacao=ano, exemplares_disponiveis=0)
+        livro.save()
+        livros = Livro.objects.all()
+        print(livros)
+        return render(request, "Biblioteca/divteste.html", {"livros": livros})
+
+    return HttpResponse("NADA FOI CRIADO.")
