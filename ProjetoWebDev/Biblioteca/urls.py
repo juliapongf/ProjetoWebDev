89d739aec22f4475b3a1ffabd469e6834cc78341
Julia Pongeluppi Franca <juliapong@go.linux.ime.usp.br>
2025-11-12 11:32:08 -0300

    Adicionando a funcionalidade de cadastrar mais de um autor para o mesmo livro

--- ProjetoWebDev/Biblioteca/urls.py
+++ ProjetoWebDev/Biblioteca/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("",       views.index,       name="index"),
-    path("gerenciar",       views.gerenciar,       name="gerenciar"),
+    path("gerenciar/",       views.gerenciar,       name="gerenciar"),
     path("criar/", views.criar_livro, name="criar_livro"),
     path('divteste/', views.index,    name='urlteste'), path("pesquisa/", views.pesquisa, name="pesquisa"),
     path("pesquisar/", views.pesquisar, name="pesquisar")
--- ProjetoWebDev/Biblioteca/views.py
+++ ProjetoWebDev/Biblioteca/views.py
@@ -15,19 +15,35 @@ def criar_livro(request):
     if request.method == "POST":
         titulo = request.POST.get("titulo")
         autor = request.POST.get("autor")
+        autores_novos = autor.split(',')
         genero = request.POST.get("genero")
         sinopse = request.POST.get("sinopse")
         ano = request.POST.get("ano")
         biografia = request.POST.get("biografia")
+        biografias_novas = biografia.split('/')
 
-        livro = Livro(titulo=titulo, genero=genero, sinopse=sinopse, ano_de_publicacao=ano, exemplares_disponiveis=0)
-        livro.save()
-        autor = Autor(nome=autor, biografia=biografia)
-        autor.save()
+        livro_existe = Livro.objects.filter(titulo=titulo).first()
+        if livro_existe:
+            return HttpResponse("O livro já existe.")
+        else:
+            livro = Livro(titulo=titulo, genero=genero, sinopse=sinopse, ano_de_publicacao=ano, exemplares_disponiveis=0)
+            livro.save()
+
+        i=0
+        for autor_novo in autores_novos:
+            autor_existe = Autor.objects.filter(nome=autor_novo).first()
+            if autor_existe:
+                autor_novo = autor_existe
+            else:
+                biografia_nova=biografias_novas[i]
+                autor_novo = Autor(nome=autor_novo, biografia=biografia_nova)
+                autor_novo.save()
+            autoria = Autoria(livro=livro, autor=autor_novo)
+            autoria.save()
+
+        
         livros = Livro.objects.all()
         autores = Autor.objects.all()
-        autoria = Autoria(livro=livro, autor=autor)
-        autoria.save()
         autorias = Autoria.objects.all()
         return render(request, "Biblioteca/divteste.html", {"livros": livros, "autores": autores, "autorias": autorias})
 
